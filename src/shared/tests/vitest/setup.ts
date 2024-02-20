601c917e2f157a0460d86655819a63a05b64286f
Carlo Ferreira <hello@carloferreira.com>
2024-02-20 17:15:01 +0100

    chore: setup Vitest and React Testing Library

---
+++ src/shared/tests/vitest/setup.ts
@@ -0,0 +1,6 @@
+/**
+ * The @testing-library/jest-dom library provides a set of custom matchers that you can use to extend Vitest matchers
+ *
+ * @see https://github.com/testing-library/jest-dom?tab=readme-ov-file#with-vitest
+ */
+import "@testing-library/jest-dom/vitest";
