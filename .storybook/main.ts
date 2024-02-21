9b6e3552637e6005f700a632776845f90c262b2a
Carlo Ferreira <hello@carloferreira.com>
2024-02-21 14:45:33 +0100

    chore(storybook): fix import resolve aliases

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/nextjs";
+import path from "path";
 
 const config: StorybookConfig = {
   addons: [
@@ -18,6 +19,25 @@ const config: StorybookConfig = {
   },
 
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
+  /**
+   * This function is the webpack final configuration step, where it resolves aliases for the paths.
+   * It modifies the webpack configuration to include aliases for easier imports.
+   *
+   * @see {@link https://storybook.js.org/docs/react/configure/webpack#extending-storybooks-webpack-config}
+   *
+   * @param {object} config - The webpack configuration object.
+   * @return {object} The updated webpack configuration object with resolved aliases.
+   */
+  webpackFinal: async (config) => {
+    config.resolve ??= {};
+
+    config.resolve.alias ??= {};
+
+    config.resolve.alias["@"] = path.resolve(__dirname, "../src");
+
+    return config;
+  },
 };
 
 export default config;
