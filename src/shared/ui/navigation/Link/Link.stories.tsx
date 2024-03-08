c35c1664a00b43612ad963d74bc1c26c7b80ce89
Anthony Desorbais <anthony@derniercri.io>
2024-03-08 15:29:07 +0100

    feat(shared): add link variants

--- src/shared/ui/navigation/Link/Link.stories.tsx
+++ src/shared/ui/navigation/Link/Link.stories.tsx
@@ -1,17 +1,23 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
+import { buttonVariants } from "../../inputs/Button/Button";
 import { Link } from "./Link";
 
 const meta = {
+  args: {
+    href: "/",
+    children: "Click me to go to home",
+  },
   component: Link,
 } satisfies Meta<typeof Link>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Default: Story = {};
+
+export const AsButton: Story = {
   args: {
-    href: "/",
-    children: "Click me to go to home",
+    className: buttonVariants({ variant: "accent" }),
   },
 };
