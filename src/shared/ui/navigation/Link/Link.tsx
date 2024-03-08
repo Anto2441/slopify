affef222f6ac1f5cf23c9297fc443915398c4ad4
Anthony Desorbais <anthony@derniercri.io>
2024-03-08 15:29:07 +0100

    feat(shared): add link variants

--- src/shared/ui/navigation/Link/Link.tsx
+++ src/shared/ui/navigation/Link/Link.tsx
@@ -1,5 +1,34 @@
+import { cva, type VariantProps } from "cva";
 import NextLink from "next/link";
 import React from "react";
+import { twMerge } from "tailwind-merge";
+
+import { focusVisibleRing } from "../../utils";
+
+const linkVariants = cva({
+  base: `${focusVisibleRing()}`,
+
+  defaultVariants: {
+    underline: `none`,
+    variant: `inherit`,
+  },
+
+  variants: {
+    underline: {
+      hover: `hover:underline`,
+      none: ``,
+    },
+
+    variant: {
+      base: ``,
+      inherit: ``,
+      muted: `text-text-subdued hover:text-text-base`,
+    },
+  },
+});
+
+type LinkProps = React.ComponentPropsWithoutRef<typeof NextLink> &
+  VariantProps<typeof linkVariants>;
 
 /**
  * Renders a link with the specified properties.
@@ -7,10 +36,14 @@ import React from "react";
  */
 export const Link = React.forwardRef<
   React.ElementRef<typeof NextLink>,
-  React.ComponentPropsWithoutRef<typeof NextLink>
->(function Link({ children, ...props }, ref) {
+  LinkProps
+>(function Link({ children, className, underline, variant, ...props }, ref) {
   return (
-    <NextLink {...props} ref={ref}>
+    <NextLink
+      {...props}
+      className={twMerge(linkVariants({ className, underline, variant }))}
+      ref={ref}
+    >
       {children}
     </NextLink>
   );
