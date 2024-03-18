f2df6c6e0aeb706847651ee9f1eac90913dff822
Anthony Desorbais <anthony@derniercri.io>
2024-03-18 12:22:49 +0100

    feat(shared): add containers queries to card list component

--- src/shared/ui/surfaces/CardList/CardList.tsx
+++ src/shared/ui/surfaces/CardList/CardList.tsx
@@ -5,12 +5,19 @@ import { Card } from "../Card/Card";
  */
 export function CardList() {
   return (
-    <div className="grid grid-cols-5 gap-spacing-looser">
-      <Card />
-      <Card roundedPicture />
-      <Card />
-      <Card />
-      <Card />
+    <div className="@container">
+      <div className="grid grid-cols-2 gap-spacing-looser @xs:grid-cols-3 @md:grid-cols-4 @lg:grid-cols-5 @xl:grid-cols-6 @2xl:grid-cols-7 @3xl:grid-cols-8 @4xl:grid-cols-9">
+        <Card />
+        <Card roundedPicture />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
