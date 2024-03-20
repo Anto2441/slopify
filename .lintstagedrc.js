6bd25fd0fe7b2a9e51cbfe6df06c596462fb940f
Carlo Ferreira <hello@carloferreira.com>
2024-03-20 17:25:16 +0100

    chore: add lint:secrets script used to prevent committing credentials
    add secrelint package
    update script names
    update lint-staged with lint:secrets task against all files

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,16 +1,16 @@
 import path from "path";
 
 /**
- * This function allows the `next lint` excutable to find and lint files in the repository
+ * This function allows the `next lint` executable to find and lint files in the repository
  */
 function lint() {
-  return `pnpm lint --file ${filenames
+  return `pnpm lint:javascript --file ${filenames
     .map((filename) => path.relative(process.cwd(), filename))
     .join(" --file ")}`;
 }
 
 /**
- * This function allows the `next test` excutable to find and lint files in the repository
+ * This function allows the `next test` executable to find and lint files in the repository
  */
 function test(filenames) {
   return `pnpm test -- --run related ${filenames
@@ -18,9 +18,17 @@ function test(filenames) {
     .join(" ")}`;
 }
 
+/**
+ * Lint staged configuration.
+ *
+ * 🦉 Each tack runs concurrently
+ *
+ * @see https://github.com/okonet/lint-staged#configuration
+ */
 export default {
+  "**/*": "pnpm lint:secrets",
   "**/*.{json,js,jsx,md,ts,tsx}": "pnpm format --check",
-  "**/*.(ts,tsx)": "pnpm types",
+  "**/*.(ts,tsx)": "pnpm lint:types",
   "**/*.{ts,tsx}": lint,
   "**/*.{ts,tsx}": test,
 };
