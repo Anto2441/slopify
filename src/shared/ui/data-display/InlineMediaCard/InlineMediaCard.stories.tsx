934ab6887818df0966dcf68343fd2a9ced4f3433
Anthony Desorbais <anthony@derniercri.io>
2024-03-20 12:52:40 +0100

    feat: requested changes - renaming and refacto on maindetails component

--- src/shared/ui/data-display/InlineMediaCard/InlineMediaCard.stories.tsx
+++ src/shared/ui/data-display/InlineMediaCard/InlineMediaCard.stories.tsx
@@ -1,18 +1,17 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { MainDetails } from "./MainDetails";
+import { InlineMediaCard } from "./InlineMediaCard";
 
 const meta = {
   args: {
-    artist: "Artist",
     picture: "https://picsum.photos/300/300",
-    pictureHeight: 56,
+    pictureSize: 56,
     pictureTopRounded: false,
-    pictureWidth: 56,
+    subtitle: "Artist",
     title: "Song title",
   },
-  component: MainDetails,
-} satisfies Meta<typeof MainDetails>;
+  component: InlineMediaCard,
+} satisfies Meta<typeof InlineMediaCard>;
 
 export default meta;
 
