eaf927f2ac1b932cc2cad67d71011375a2d1f0ee
Anthony Desorbais <anthony@derniercri.io>
2024-03-27 11:19:09 +0100

    feat: refacto - remove unnecessary components and adjust styling

--- src/shared/ui/navigation/DropdownMenu/DropdownMenu.tsx
+++ src/shared/ui/navigation/DropdownMenu/DropdownMenu.tsx
@@ -1,66 +1,28 @@
 "use client";
 
 import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
-import { HiChevronRight } from "react-icons/hi2";
-import { IoMdCheckmark } from "react-icons/io";
-import { RxDotFilled } from "react-icons/rx";
-
 import * as React from "react";
-
+import { IoMdCheckmark } from "react-icons/io";
 import { twJoin } from "tailwind-merge";
 
+/**
+ * Root component of a dropdown menu.
+ *
+ * @see https://www.radix-ui.com/docs/primitives/components/dropdown-menu
+ */
 export const DropdownMenu = DropdownMenuPrimitive.Root;
 
+/**
+ * Component that opens the dropdown menu.
+ * This component should be used as a trigger for the dropdown menu. When
+ * clicked, it will open the dropdown menu.
+ */
 export const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
-export const DropdownMenuGroup = DropdownMenuPrimitive.Group;
 
-export const DropdownMenuPortal = DropdownMenuPrimitive.Portal;
-
-export const DropdownMenuSub = DropdownMenuPrimitive.Sub;
-
-export const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup;
-
-export const DropdownMenuSubTrigger = React.forwardRef<
-  React.ElementRef<typeof DropdownMenuPrimitive.SubTrigger>,
-  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.SubTrigger> & {
-    inset?: boolean;
-  }
->(function DropdownMenuSubTrigger(
-  { className, inset, children, ...props },
-  ref,
-) {
-  return (
-    <DropdownMenuPrimitive.SubTrigger
-      ref={ref}
-      className={twJoin(
-        "focus:bg-accent data-[state=open]:bg-accent flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none",
-        inset && "pl-8",
-        className,
-      )}
-      {...props}
-    >
-      {children}
-      <HiChevronRight className="ml-auto h-4 w-4" />
-    </DropdownMenuPrimitive.SubTrigger>
-  );
-});
-
-export const DropdownMenuSubContent = React.forwardRef<
-  React.ElementRef<typeof DropdownMenuPrimitive.SubContent>,
-  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.SubContent>
->(function DropdownMenuSubContent({ className, ...props }, ref) {
-  return (
-    <DropdownMenuPrimitive.SubContent
-      ref={ref}
-      className={twJoin(
-        "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] overflow-hidden rounded-md border p-1 shadow-lg",
-        className,
-      )}
-      {...props}
-    />
-  );
-});
-
+/**
+ * Component that displays the dropdown menu content.
+ * This component is opened when the `DropdownMenuTrigger` is clicked.
+ */
 export const DropdownMenuContent = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
@@ -71,20 +33,24 @@ export const DropdownMenuContent = React.forwardRef<
   return (
     <DropdownMenuPrimitive.Portal>
       <DropdownMenuPrimitive.Content
-        ref={ref}
+        {...props}
         align={align}
         sideOffset={sideOffset}
         className={twJoin(
-          "z-50 min-w-[12.25rem] overflow-hidden rounded-border-radius-base bg-background-elevated-base p-spacing-tighter-4 text-color-base shadow-md",
-          "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+          "min-w-[12.25rem] overflow-hidden rounded-border-radius-base bg-background-elevated-base p-spacing-tighter-4 text-color-base shadow-md",
           className,
         )}
-        {...props}
+        ref={ref}
       />
     </DropdownMenuPrimitive.Portal>
   );
 });
 
+/**
+ * Component that renders a single item in the dropdown menu.
+ * This component should not be used directly. Instead, use one of the other
+ * components that are meant to be used as children of `DropdownMenuContent`.
+ */
 export const DropdownMenuItem = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Item> & {
@@ -93,17 +59,21 @@ export const DropdownMenuItem = React.forwardRef<
 >(function DropdownMenuItem({ className, inset, ...props }, ref) {
   return (
     <DropdownMenuPrimitive.Item
-      ref={ref}
+      {...props}
       className={twJoin(
-        "relative flex cursor-default select-none items-center rounded-sm py-2.5 pl-spacing-tighter pr-spacing-tighter-2 text-font-size-smaller outline-none transition-colors focus:bg-background-tinted-highlight data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-        inset && "pl-8",
+        "relative flex cursor-default select-none items-center rounded-border-radius-smaller py-spacing-tighter-2 pl-spacing-tighter pr-spacing-tighter-2 text-font-size-smaller outline-none focus:bg-background-tinted-highlight",
+        inset && "pl-spacing-looser-2",
         className,
       )}
-      {...props}
+      ref={ref}
     />
   );
 });
 
+/**
+ * Component that renders a single checkbox item in the dropdown menu.
+ * This component should be used as a child of `DropdownMenuContent`.
+ */
 export const DropdownMenuCheckboxItem = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.CheckboxItem>,
   React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.CheckboxItem>
@@ -113,17 +83,17 @@ export const DropdownMenuCheckboxItem = React.forwardRef<
 ) {
   return (
     <DropdownMenuPrimitive.CheckboxItem
-      ref={ref}
+      {...props}
       className={twJoin(
-        "focus:bg-accent focus:text-accent-foreground relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
+        "relative flex cursor-default select-none items-center rounded-border-radius-smaller py-spacing-tighter-3 pl-spacing-looser-2 pr-spacing-tighter-2 text-font-size-smaller outline-none focus:text-color-bright-accent",
         className,
       )}
       checked={checked}
-      {...props}
+      ref={ref}
     >
-      <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+      <span className="absolute left-spacing-tighter-2 flex h-spacing-base w-spacing-base items-center justify-center">
         <DropdownMenuPrimitive.ItemIndicator>
-          <IoMdCheckmark className="h-4 w-4" />
+          <IoMdCheckmark className="h-spacing-base w-spacing-base" />
         </DropdownMenuPrimitive.ItemIndicator>
       </span>
       {children}
@@ -131,29 +101,10 @@ export const DropdownMenuCheckboxItem = React.forwardRef<
   );
 });
 
-export const DropdownMenuRadioItem = React.forwardRef<
-  React.ElementRef<typeof DropdownMenuPrimitive.RadioItem>,
-  React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.RadioItem>
->(function DropdownMenuRadioItem({ className, children, ...props }, ref) {
-  return (
-    <DropdownMenuPrimitive.RadioItem
-      ref={ref}
-      className={twJoin(
-        "focus:bg-accent focus:text-accent-foreground relative flex cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none transition-colors data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-        className,
-      )}
-      {...props}
-    >
-      <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
-        <DropdownMenuPrimitive.ItemIndicator>
-          <RxDotFilled className="h-4 w-4 fill-current" />
-        </DropdownMenuPrimitive.ItemIndicator>
-      </span>
-      {children}
-    </DropdownMenuPrimitive.RadioItem>
-  );
-});
-
+/**
+ * Component that renders a single label item in the dropdown menu.
+ * This component should be used as a child of `DropdownMenuContent`.
+ */
 export const DropdownMenuLabel = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Label>,
   React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Label> & {
@@ -162,40 +113,33 @@ export const DropdownMenuLabel = React.forwardRef<
 >(function DropdownMenuLabel({ className, inset, ...props }, ref) {
   return (
     <DropdownMenuPrimitive.Label
-      ref={ref}
-      className={twJoin("px-2 py-1.5 text-sm", inset && "pl-8", className)}
       {...props}
+      className={twJoin(
+        "px-spacing-tighter-2 py-spacing-tighter-2 text-font-size-smaller",
+        inset && "pl-spacing-looser-2",
+        className,
+      )}
+      ref={ref}
     />
   );
 });
 
+/**
+ * Component that renders a separator item in the dropdown menu.
+ * This component should be used as a child of `DropdownMenuContent`.
+ */
 export const DropdownMenuSeparator = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Separator>,
   React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Separator>
 >(function DropdownMenuSeparator({ className, ...props }, ref) {
   return (
     <DropdownMenuPrimitive.Separator
-      ref={ref}
+      {...props}
       className={twJoin(
         "border-b border-background-tinted-highlight",
         className,
       )}
-      {...props}
+      ref={ref}
     />
   );
 });
-
-export function DropdownMenuShortcut({
-  className,
-  ...props
-}: React.HTMLAttributes<HTMLSpanElement>) {
-  return (
-    <span
-      className={twJoin(
-        "ml-auto text-xs tracking-widest opacity-60",
-        className,
-      )}
-      {...props}
-    />
-  );
-}
--- src/shared/ui/navigation/index.ts
+++ src/shared/ui/navigation/index.ts
@@ -2,17 +2,9 @@ export {
   DropdownMenu,
   DropdownMenuCheckboxItem,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
   DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "./DropdownMenu/DropdownMenu";
 export { Link } from "./Link/Link";
