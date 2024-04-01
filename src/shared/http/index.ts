ac3aacca99e22dff0ca20e916941e8ba44444592
Carlo Ferreira <hello@carloferreira.com>
2024-04-01 19:11:26 +0200

    feat(shared): add http service
    The http service will be responsible for making HTTP requests in our app

---
+++ src/shared/http/index.ts
@@ -0,0 +1,8 @@
+export { HttpService } from "./HttpService";
+export type { IHttpService } from "./IHttpService";
+export type {
+  HttpError,
+  HttpOptions,
+  HttpResource,
+  HttpResponse,
+} from "./types";
